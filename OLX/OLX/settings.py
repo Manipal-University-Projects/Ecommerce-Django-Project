fde027d1cd8c6656ee1355b9390a9ecfabf8f47d
Kaustav Ghosh <teetangh@gmail.com>
2021-03-26 16:03:04 +0530

    top picks and new arrivals

--- OLX/OLX/settings.py
+++ OLX/OLX/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7ui7%13cl$)wmez9qq(of3#-1+&c)b^7s+67zu#v2s6-z)f2*6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1.xip.io']
+ALLOWED_HOSTS = []
 
 
 # Application definition
