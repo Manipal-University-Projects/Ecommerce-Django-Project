6e34340cf09db517204b2790d53e1c0c4a6d149a
Kaustav Ghosh <teetangh@gmail.com>
2021-03-27 17:40:05 +0530

    working on navbar

--- OLX/Home/urls.py
+++ OLX/Home/urls.py
@@ -5,4 +5,7 @@ from Home import views
 
 urlpatterns = [
     path('', views.index),
+    path('/contact', views.contact),
+    path('/about-us', views.about_us),
+    path('/privacy-policy', views.privacy_policy),
 ]
--- OLX/Home/views.py
+++ OLX/Home/views.py
@@ -4,5 +4,17 @@ from django.shortcuts import render
 
 
 def index(request):
+    context = {'data': [i for i in range(10)]}
+    return render(request, 'Home/home_page.html', context=context)
 
-    return render(request, 'Home/home_page.html')
+
+def contact(request):
+    return render(request, "Home/contact.html")
+
+
+def about_us(request):
+    return render(request, "Home/about_us.html")
+
+
+def privacy_policy(request):
+    return render(request, "Home/privacy_policy.html")
